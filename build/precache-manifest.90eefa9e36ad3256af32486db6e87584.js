dc40c65f51ef98068ef8a6019eedd0f48ad18787
ismail rabah <ismailrabahforboy01@gmail.com>
2019-12-26 05:23:53 +0100

    commite_02_there_is_reasons

---
+++ build/precache-manifest.90eefa9e36ad3256af32486db6e87584.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d2922d408cd7f65134435cb12af0e9",
+    "url": "/Get_car/index.html"
+  },
+  {
+    "revision": "89018bc5d5aedce5bbc0",
+    "url": "/Get_car/static/css/2.58bd2952.chunk.css"
+  },
+  {
+    "revision": "778eca50e16022d73596",
+    "url": "/Get_car/static/css/main.206c6f70.chunk.css"
+  },
+  {
+    "revision": "89018bc5d5aedce5bbc0",
+    "url": "/Get_car/static/js/2.5f37dc49.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/Get_car/static/js/2.5f37dc49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "778eca50e16022d73596",
+    "url": "/Get_car/static/js/main.dde9b92b.chunk.js"
+  },
+  {
+    "revision": "8dec7ebd184a2062dfbe",
+    "url": "/Get_car/static/js/runtime-main.82acccaa.js"
+  },
+  {
+    "revision": "f66a0149524a231130604372d8d58e6d",
+    "url": "/Get_car/static/media/Jumbotron.f66a0149.png"
+  },
+  {
+    "revision": "528579d954c149d0ca5d06595b82f6f0",
+    "url": "/Get_car/static/media/about_us.528579d9.jpg"
+  },
+  {
+    "revision": "3fb48e866b685a9015fe59e68aa57af7",
+    "url": "/Get_car/static/media/business_insider_logo.3fb48e86.png"
+  },
+  {
+    "revision": "9d330f52a61c2dd9c137db640bdcedb4",
+    "url": "/Get_car/static/media/car1.9d330f52.png"
+  },
+  {
+    "revision": "f66a0149524a231130604372d8d58e6d",
+    "url": "/Get_car/static/media/car2.f66a0149.png"
+  },
+  {
+    "revision": "8a2480b435b9b6de3ac9eee6d2618b49",
+    "url": "/Get_car/static/media/car3.8a2480b4.png"
+  },
+  {
+    "revision": "41e5948f259b14efce0ee8a07984cca6",
+    "url": "/Get_car/static/media/curbed_logo.41e5948f.png"
+  },
+  {
+    "revision": "790ac652f17494f7e031b53aeb1ea8ff",
+    "url": "/Get_car/static/media/logo192.790ac652.png"
+  },
+  {
+    "revision": "011fce22fc29bed2cc08710b61cfd7ff",
+    "url": "/Get_car/static/media/metropolice_logo.011fce22.png"
+  },
+  {
+    "revision": "ac9082f66f97089f3b09c4784ab2b7a0",
+    "url": "/Get_car/static/media/sam.ac9082f6.jpg"
+  },
+  {
+    "revision": "841ae283faeb82759ae7681b5996378b",
+    "url": "/Get_car/static/media/service_zone.841ae283.png"
+  },
+  {
+    "revision": "c5269f137b3c63163faca696c5e08bf7",
+    "url": "/Get_car/static/media/urban_x_logo.c5269f13.png"
+  },
+  {
+    "revision": "20f64da4ad27ea0eaa2878a40260d6ae",
+    "url": "/Get_car/static/media/wired_logo.20f64da4.png"
+  }
+]);
